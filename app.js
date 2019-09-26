d2410514921d45e9a07a5995108afd7191abbfcf
Agnieszka Dwulit <aga.i.dwulit@gmail.com>
2019-09-26 16:32:36 -0400

    Completed Sequelize Essentials

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const morgan = require("morgan");
+const morgan = require('morgan');
+const {db} = require('./models');
+
 // const client = require("./db");
 // const postList = require("./views/postList");
 // const postDetails = require("./views/postDetails");
+
 
 
 const app = express();
@@ -17,3 +20,24 @@ app.use('/static', express.static(__dirname + "/public"));
 //   app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, "public")))
+app.get('/', (req, res) => {
+    res.send('')
+  });
+
+// db.authenticate().
+// then(() => {
+//     console.log('connected to the database');
+// })
+const PORT = 3000;
+
+const init = async () => {
+  await db.sync({force: true})
+  console.log(db.models.page);
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+  });
+
+}
+ 
+init();
