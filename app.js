35ec9e8efc635c1347ea19f86f1224454c5f2ea3
LinaD <lina.dinh262@gmail.com>
2019-09-26 14:22:50 -0400

    create files and forderS

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const morgan = require(./morgan)
+const express = require("express");
+const morgan = require("morgan");
+// const client = require("./db");
+// const postList = require("./views/postList");
+// const postDetails = require("./views/postDetails");
+
+
+const app = express();
+
+// const routes = require('./routes/posts');
+// app.use('/posts', routes);
+
+app.use(morgan("dev"));
+app.use('/static', express.static(__dirname + "/public"));
+
+// app.use(express.urlencoded({ extended: false }));
+//   app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, "public")))
